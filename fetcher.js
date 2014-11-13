f7fe8b93d32eb37282b38f10519f12fa556a6878
Prateek Jain <prateek.jain23995@yahoo.com>
2014-11-13 19:16:52 +0530

    Prateek | functionality implemented to parse and fetch feeds and storing in local variable.

---
+++ fetcher.js
@@ -0,0 +1,29 @@
+var feeds = {};
+var googleFeedApiUrl = "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=-1&q=";
+
+
+var fetchCategories = function(categories){
+	for (var categoryName in categories){
+		var category = categories[categoryName];
+		feeds[categoryName] = {feeds : []};
+
+		category.forEach(function(feedDetail){
+			function getFeed(categoryName){
+				downLoadJsonFrom(googleFeedApiUrl + feedDetail.url, "jsonp", function(feed){ 
+					feeds[categoryName].feeds.push(feed); 
+				});
+			}
+			getFeed(categoryName)
+		});
+
+	}
+};
+
+var downLoadJsonFrom = function(url, dataType, success){
+	$.ajax({url : url, dataType: dataType, success : success})
+}
+
+
+
+var gistUrl = "https://gist.githubusercontent.com/jainsahab/aab127d9cc1a0ec7e90e/raw/a84640ca9c70ac73c61ea206a54e9e8843a07f0f/feeds-details"
+downLoadJsonFrom(gistUrl, "json", fetchCategories)
