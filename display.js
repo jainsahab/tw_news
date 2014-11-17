2594dafd52ceffb2c4afa7ac7028eb04b74b7b8d
Prateek Jain <prateek.jain23995@yahoo.com>
2014-11-17 13:40:44 +0530

    Prateek, Soumya : fetching continuous feeds after a specific time interval.

--- display.js
+++ display.js
@@ -6,11 +6,13 @@ var sorter = function(first,second){
 }
 
 var getAllFeedFor = function(category){
-	if(category.feeds.length <= 1) return category.feeds;
+	if(category.feeds.length == 0) return [];
+	if(category.feeds.length == 1) return category.feeds[0].responseData.feed.entries;
 	return category.feeds.reduce(function(pv, cv){
 		return pv.responseData.feed.entries.concat(cv.responseData.feed.entries)
 	});
 }
+
 
 var deleteUnnecessaryFields = function(feed){
 	var unNecessaryFields = ["author", "contentSnippet", "categories"];
@@ -18,6 +20,15 @@ var deleteUnnecessaryFields = function(feed){
 		delete feed[field];
 	})
 	return feed;
+}
+
+var getOnlyNecessaryFields = function(feed){
+	var feedCopy = {};
+	var necessaryFields = ["title", "link", "publishedDate", "content"];
+	necessaryFields.forEach(function(field){
+		feedCopy[field] = feed[field];
+	})
+	return feedCopy;
 }
 
 var sortAndDisplayFeeds = function (allFeeds) {
@@ -38,6 +49,7 @@ var getHtmlFor = function(feed){
 }
 
 var display = function(sortedFeeds, categoryName){
+	if ( sortedFeeds.length == 0) return;
 	var headingHtml = '<div id="'+categoryName+'"><div class = "heading">' + categoryName + '</div></div>';
 	$("#container").append(headingHtml)
 
@@ -46,7 +58,7 @@ var display = function(sortedFeeds, categoryName){
 	})
 };	
 
-$( document ).ajaxComplete(function() {
-	$("#container").empty();
-  	sortAndDisplayFeeds(feeds);
-});
+// $( document ).ajaxComplete(function() {
+// 	$("#container").empty();
+//   	sortAndDisplayFeeds(feeds);
+// });
--- fetcher.js
+++ fetcher.js
@@ -1,24 +1,33 @@
 var feeds = {};	
-var googleFeedApiUrl = "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=-1&q=";
+var googleFeedApiUrl = "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&q=";
 var gistUrl = "https://gist.githubusercontent.com/jainsahab/aab127d9cc1a0ec7e90e/raw/a84640ca9c70ac73c61ea206a54e9e8843a07f0f/feeds-details"
 
 var fetchCategories = function(categories){
-	for (var categoryName in categories){
+
+	Object.keys(categories).forEach(function(categoryName, categoryIndex, categoryArr){
 		var category = categories[categoryName];
 		feeds[categoryName] = {feeds : []};
 
-		category.forEach(function(feedDetail){
-			function getFeedFor(categoryName){
+		category.forEach(function(feedDetail, index, arr){
+			function getFeedFor(categoryName, categoryIndex, index){
 				downLoadJsonFrom(googleFeedApiUrl + feedDetail.url, "jsonp", function(feed){ 
 					feeds[categoryName].feeds.push(feed); 
+					if( categoryArr.length == categoryIndex + 1 && arr.length == index + 1){
+						$("#container").empty();
+					  	sortAndDisplayFeeds(feeds);
+					}
 				});
 			}
-			getFeedFor(categoryName)
+			getFeedFor(categoryName, categoryIndex, index)
 		});
-
-	}
+	})
 };
 
 var downLoadJsonFrom = function(url, dataType, success){
 	$.ajax({url : url, dataType: dataType, success : success})
 }
+
+
+setInterval(function(){
+	downLoadJsonFrom(gistUrl, 'json', fetchCategories);
+}, 3000);
